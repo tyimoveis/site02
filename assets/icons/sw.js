02f575ee9a8dd45cf2face0548f825d1aa426c62
app. <imoveisty@gmail.com>
2025-05-10 09:31:46 -0300

    sw.js

---
+++ assets/icons/sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'entehome-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/css/style.css',
+  '/assets/js/main.js',
+  '/assets/icons/icon-192x192.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
